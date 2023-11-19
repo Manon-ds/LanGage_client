c87fab13762e60c9e6a8943488067ec78c0a3dbd
Rich B <richardbaker1993@gmail.com>
2023-11-19 18:34:51 +0000

    feat: almost refactored Navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,16 +1,25 @@
+import React,{useState, ReactElement, ReactNode} from "react";
 import { ReactComponent as Logo } from "../assets/Logo no slogan.svg";
-import Hamburger from "./Hamburger.jsx";
+import Hamburger from "./Hamburger";
 import Popup from "reactjs-popup";
-import { useState } from "react";
+
+type NavProps = {
+  conversation: number;
+  setConversation: (value: number) => void;
+  conversationList: number[];
+  setConversationList: (value: number) => void;
+};
 
 function Nav({
   conversation,
   setConversation,
   conversationList,
   setConversationList,
-}) {
+}: NavProps)   {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-// TODO Combine into one function.
+  // TODO Combine into one function.
+
+
   const handleOpen = () => {
     setIsMenuOpen(true);
   };
@@ -19,16 +28,35 @@ function Nav({
     setIsMenuOpen(false);
   };
 
+  const popupContent: ReactNode = (
+    <div data-testid="popMenu" className="popUpMenu">
+    <Hamburger
+      conversation={conversation}
+      setConversation={setConversation}
+      conversationList={conversationList}
+      setConversationList={setConversationList}
+    />
+    <div className="closeButtonContainer">
+      <button className="closeButton" onClick={() => close()}>
+        Close
+      </button>
+    </div>
+  </div>
+
+  )
+
+
+
   return (
     <div className="Nav">
       <div className="logoContainer">
-        <Logo data-testid="logo"  className="logo" />
+        <Logo data-testid="logo" className="logo" />
       </div>
 
       <Popup
         className="popUpComponent"
         trigger={
-          <button data-testid='hamburger' className="hamburgerButton">
+          <button data-testid="hamburger" className="hamburgerButton">
             <div className={`hamburger ${isMenuOpen ? "active" : ""}`}>
               <span className="bar"></span>
               <span className="bar"></span>
@@ -40,21 +68,7 @@ function Nav({
         onClose={handleClose}
         position="bottom center"
       >
-        {(close) => (
-            <div data-testid = 'popMenu' className="popUpMenu">
-            <Hamburger
-              conversation={conversation}
-              setConversation={setConversation}
-              conversationList={conversationList}
-              setConversationList={setConversationList}
-            />
-            <div className="closeButtonContainer">
-              <button className="closeButton" onClick={() => close()}>
-                Close
-              </button>
-            </div>
-          </div>
-        )}
+        {popupContent}
       </Popup>
     </div>
   );
