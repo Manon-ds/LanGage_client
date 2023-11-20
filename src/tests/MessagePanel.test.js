e0017812306eae715a007032a67b0d4457692a86
Manon <manonbranc@outlook.fr>
2023-11-20 16:53:07 +0100

    tests message pannel

--- src/tests/MessagePanel.test.js
+++ src/tests/MessagePanel.test.js
@@ -1,14 +1,40 @@
 /* eslint-disable no-undef */
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import MessagePanel from "../components/MessagePanel";
+// import Messages from "../components/Messages";
+import Form from "../components/Form";
+import userEvent from '@testing-library/user-event'
 
-describe.skip(MessagePanel, () => {
+
+describe.only(MessagePanel, () => {
   it('Should render a send button', () => {
-    const { getByClassName } = render(
+    const { getByTestId } = render(
       <MessagePanel />
     );
-    const sendButton = getByClassName('send-button');
+    const sendButton = getByTestId('submitButton');
     expect(sendButton).toBeInTheDocument();
   })
+  it('Should render logo', async () => {
+    const { getByTestId } = render( <MessagePanel/>);
+    const logo= getByTestId('logo')
+    expect(logo).toBeInTheDocument();
+  })
+  it('should set input value when message is typed', () => {
+    const { getByTestId } = render(<MessagePanel/> );
+    userEvent.type(getByTestId('input'), 'Hello,World!')
+    expect(getByTestId('input')).toHaveValue('Hello,World!')
+  })
+  it('calls the onSubmit callback handler', () => {
+    const handleSubmit = jest.fn();
+    const handleChange = jest.fn();
+
+    const { getByTestId } = render(
+      <Form handleSubmit={handleSubmit} handleChange={handleChange} input={'this is a message'} loading={false}/>
+    );
+    fireEvent.submit(getByTestId('form'),
+    );
+
+    expect(handleSubmit).toHaveBeenCalled();
+  });
 
 })
